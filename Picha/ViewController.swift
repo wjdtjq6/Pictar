a2b5a5143a966424cd352b44e4448ac8e40a3193
jimmy <wjdtjq6@gmail.com>
2024-07-22 14:28:35 +0900

    Initial Commit

---
+++ Picha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picha
+//
+//  Created by t2023-m0032 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
